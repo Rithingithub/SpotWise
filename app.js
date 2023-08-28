cb267c61e93f557fbdc5113145f96f9227c7c5d2
WazeenX <wazeen999@gmail.com>
2023-08-28 19:51:07 +0530

    Updated env and app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { Client } = require('pg');
+
 const app = express();
-require('dotenv').config(); 
+require('dotenv').config();
 
 const routers = require('./router/router');
 
@@ -10,7 +12,32 @@ app.use(express.json());
 // Use the router
 app.use('/api/v1', routers);
 
-const PORT = process.env.PORT; // Use the PORT variable from environment or default to 3000
+// Database connection configuration
+const dbClient = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "1234",
+  database: "postgres"
+});
+
+// Endpoint to retrieve user IDs and usernames
+app.get('/api/v1/users', (req, res) => {
+  dbClient.connect();
+
+  dbClient.query(`SELECT id, username FROM users`, (err, dbRes) => {
+    if (!err) {
+      res.json(dbRes.rows);
+    } else {
+      console.log(err.message);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+
+    dbClient.end();
+  });
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
