76f790730b91754ff68753e52f162a249dff55de
falah <falahmpm@gmail.com>
2023-09-07 18:37:46 +0530

    login+otp

--- client/src/pages/OTPVerification.js
+++ client/src/pages/OTPVerification.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import prjLogo from '../images/icon_car.png';
 import { resendCode, consumeCode } from "supertokens-web-js/recipe/passwordless";
+import { useLocation } from 'react-router-dom'; // Import useLocation
+import prjLogo from '../images/icon_car.png';
 
 const OTPVerification = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const phoneNumber = queryParams.get('phoneNumber');
 
+  // Create separate state variables for each OTP input field
   const [otp1, setOtp1] = useState('');
   const [otp2, setOtp2] = useState('');
   const [otp3, setOtp3] = useState('');
@@ -69,6 +72,26 @@ const OTPVerification = () => {
     }
   }
 
+
+  const handleOtpVerification = (e) => {
+    e.preventDefault();
+
+    // Combine the separate OTP values into one OTP string
+    const otp = otp1 + otp2 + otp3 + otp4;
+
+    // Validate the OTP here (you can add your logic for OTP validation)
+    const isOtpValid = otp === '1234'; // Replace '1234' with your actual OTP validation logic
+
+    if (isOtpValid) {
+      // If OTP is valid, navigate to the main page
+      // You can use window.location.href or any routing library for navigation
+      window.location.href = '/MainPage'; // This is a simple example using window.location.href
+    } else {
+      // Handle OTP validation failure (e.g., display an error message)
+      alert('Invalid OTP. Please try again.');
+    }
+  };
+
   return (
     <div>
       <div className='Header'>
@@ -81,6 +104,7 @@ const OTPVerification = () => {
       
         <form onSubmit={handleOtpVerification}>
         <label>Enter 6-digit verification code <br /><br /><br /></label>
+        <label>Enter verification code <br /><br /><br /></label>
           <div className='Vinput-container'>
             <input
               type="text"
@@ -146,6 +170,12 @@ const OTPVerification = () => {
             <div><p>Didn't receive the code?<span className='green-text' onClick={resendOTP}> Resend</span></p></div>
             <br /><br />
             <button type="submit">Verify OTP</button>
+          <div  >
+            <br /><br /><br />
+            <div><p>Did'nt recieve code?<span className='green-text' > Resend</span></p></div>
+            <br /><br />
+            <button type="submit">Verify OTP</button>
+          
           </div>
         </form>
       </div>
