5220cd9eb15bb780bc5bb677fe7bbacd5aeb96cf
LEHANS <latheeflathuthekkil@gmail.com>
2023-11-23 20:43:17 +0530

    map and geolocation implemented

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,63 +1,125 @@
 import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
-import prjLogo from '../images/icon_car.png';
-import '../App.css';
+import { useSpring, animated } from 'react-spring';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import Navbar from '../components/Navbar';
 
+const mapContainerStyle = {
+  height: '100vh',
+  width: '100%',
+};
+
+const defaultCenter = { lat: 0, lng: 0 }; // Initial location
+
 const MainPage = () => {
-  const [currentLocation, setCurrentLocation] = useState(null);
+  const [currentLocation, setCurrentLocation] = useState(defaultCenter);
+  const [isInsideBox, setIsInsideBox] = useState(false);
+  const [locationPermission, setLocationPermission] = useState(null);
+
+  const markerSpring = useSpring({
+    to: { lat: currentLocation.lat, lng: currentLocation.lng },
+    config: { tension: 180, friction: 12 },
+  });
 
   useEffect(() => {
-    // Check if the Geolocation API is available
-    if (navigator.geolocation) {
-      // Get the current location
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setCurrentLocation({ latitude, longitude });
-        },
-        (error) => {
-          console.error('Error getting location:', error.message);
-        }
-      );
+    const askForLocationPermission = async () => {
+      try {
+        await navigator.permissions.request({ name: 'geolocation' });
+        setLocationPermission('granted');
+      } catch (error) {
+        console.error('Error requesting location permission:', error);
+        setLocationPermission('denied');
+      }
+    };
 
-      // Watch for changes in the device's location
+    askForLocationPermission();
+  }, []);
+
+  useEffect(() => {
+    if (locationPermission === 'granted') {
       const watchId = navigator.geolocation.watchPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-          setCurrentLocation({ latitude, longitude });
+          setCurrentLocation({ lat: latitude, lng: longitude });
+          checkInsideBox({ lat: latitude, lng: longitude });
         },
         (error) => {
           console.error('Error watching location:', error.message);
         }
       );
 
-      // Cleanup the watcher when the component unmounts
       return () => {
         navigator.geolocation.clearWatch(watchId);
       };
-    } else {
-      console.error('Geolocation is not supported by your browser.');
     }
-  }, []); // Empty dependency array means this effect runs once when the component mounts
+  }, [locationPermission]);
+
+  const checkInsideBox = ({ lat, lng }) => {
+    // Define the coordinates of the box
+    const boxCoordinates = {
+      top: 40,
+      right: -80,
+      bottom: -10,
+      left: -150,
+    };
+
+    // Check if the current location is inside the box
+    setIsInsideBox(lat > boxCoordinates.bottom && lat < boxCoordinates.top && lng > boxCoordinates.left && lng < boxCoordinates.right);
+  };
+
+  const handleGetCurrentLocation = () => {
+    if (locationPermission === 'granted') {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setCurrentLocation({ lat: latitude, lng: longitude });
+        },
+        (error) => {
+          console.error('Error getting current location:', error.message);
+        }
+      );
+    }
+  };
 
   return (
     <div>
       <div className='Navbar'>
-        <React.Fragment>
-          <Navbar />
-        </React.Fragment>
+        <Navbar />
       </div>
 
-      <div className='map-container'>
-        {/* Display the current location or a default location */}
-        {currentLocation ? (
-          <FontAwesomeIcon icon={faMapMarkerAlt} size="5x" color="blue" />
-        ) : (
-          <p>Loading location...</p>
-        )}
-      </div>
+      <LoadScript googleMapsApiKey="AIzaSyBKFGFSP7NNoNkxVu4jDgE8Qd6_gfMwO-I">
+        <GoogleMap mapContainerStyle={mapContainerStyle} center={currentLocation} zoom={8}>
+          {locationPermission === 'granted' && (
+            <>
+              <animated.div
+                style={{
+                  position: 'absolute',
+                  left: `${markerSpring.lng + 180}px`,
+                  top: `${90 - markerSpring.lat}px`,
+                }}
+              >
+                <Marker position={{ lat: markerSpring.lat, lng: markerSpring.lng }} />
+              </animated.div>
+              <button className="get-location-button" onClick={handleGetCurrentLocation}>
+                Get My Exact Location
+              </button>
+            </>
+          )}
+        </GoogleMap>
+      </LoadScript>
+
+      <style jsx>{`
+        .map-container {
+          background-color: black;
+          position: relative;
+        }
+
+        .get-location-button {
+          position: absolute;
+          top: 16px;
+          left: 16px;
+          z-index: 1;
+        }
+      `}</style>
     </div>
   );
 };
