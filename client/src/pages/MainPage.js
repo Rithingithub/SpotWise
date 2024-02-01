22b9e4a831f90c7a7de33623930d97db18602047
falah <falahmpm@gmail.com>
2024-02-01 07:17:04 +0530

    location symbol added

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import './box.css';
+import location from '../images/locationicon.png'
 
 const BoxComponent = ({ content }) => {
   return (
@@ -17,22 +18,22 @@ const MainPage = () => {
         <Navbar />
       </div>
 
-      <div className="box-container">
-        <BoxComponent content="A1"/>
-        <BoxComponent content="A2"/>
-        <BoxComponent content="A3"/>
-        <BoxComponent content="A4"/>
-        <BoxComponent content="A5" />
-        <BoxComponent content="A6" />
-        <BoxComponent content="A7" />
-        <BoxComponent content="A8" />
-        <BoxComponent content="A9" />
-        <BoxComponent content="A10" />
-        <BoxComponent content="A11" />
-        <BoxComponent content="A12" />
+      <div className="basket-container">
+        <div className="box-container">
+          <BoxComponent content="A1" />
+          <BoxComponent content="A2" />
+          <BoxComponent content="A3" />
+          <BoxComponent content="A4" />
+          <BoxComponent content="A5" />
+          <BoxComponent content="A6" />
+          <BoxComponent content="A7" />
+          <BoxComponent content="A8" />
+          <BoxComponent content="A9" />
+          <BoxComponent content="A10" />
+          
+        </div>
+        <div className="location-symbol"><img src={location} alt="Location Icon" /></div>
       </div>
-
-      
     </div>
   );
 };
