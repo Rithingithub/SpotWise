e40fbc7e56b87016a60ed4ed94709f6ec399fea2
Rithingithub <rithinrithu1439@gmail.com>
2024-05-11 16:57:18 +0530

    fix: navbar item bug fixed

--- client/src/components/settings.js
+++ client/src/components/settings.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IoChevronBackCircleOutline } from 'react-icons/io5'; // Import the IoChevronBackCircleOutline icon
+import { IoChevronBackCircleOutline } from 'react-icons/io5';  // Import the IoChevronBackCircleOutline icon
 import css_styles from "../components/style.module.css";
 import prjLogo from '../images/icon_car.png';
 
@@ -8,17 +8,17 @@ const settings = () => {
    
     
       const goBack = () => {
-        // Use a regular anchor tag to navigate to the main page
-        window.location.href = '/MainPage';
+          // Use a regular anchor tag to navigate to the main page
+        window.location.href = '/';
       };
       return (
         <div>
-          <div className={css_styles['Header']}>
-            <img src={prjLogo} alt="Logo" width={55} height={35} />
+          <div className = {css_styles['Header']}>
+          <img src       = {prjLogo} alt = "Logo" width = {55} height = {35} />
             <h2>SpotWise</h2>
           </div>
-          <button className={css_styles["back-button"]} onClick={goBack}>
-            <IoChevronBackCircleOutline className={css_styles['icon']}/> {/* Use the IoChevronBackCircleOutline icon */}
+          <button                     className = {css_styles["back-button"]} onClick = {goBack}>
+          <IoChevronBackCircleOutline className = {css_styles['icon']}/> {/* Use the IoChevronBackCircleOutline icon */}
           </button>
         </div>
       );
